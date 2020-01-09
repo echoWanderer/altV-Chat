4f0171d6d2a17649a51c2501c0970730903ade25
Wanderer <vitkusdominykas@gmail.com>
2020-01-09 18:48:36 +0200

    Initial commit

---
+++ client.mjs
@@ -0,0 +1,82 @@
+import * as alt from 'alt';
+
+let view = new alt.WebView('http://resource/html/index.html');
+
+let chatVisible = true;
+let inputVisible = false;
+
+alt.onServer('chat:Broadcast', (senderName, text) => {
+	addChatMessage(`${senderName} says: ${text}`);
+});
+
+alt.onServer('addChatMessage', addChatMessage);
+export function addChatMessage(text) {
+	view.emit('addChatMessage', text);
+}
+
+alt.onServer('clearChat', clearChat);
+export function clearChat() {
+	view.emit('clearChatBox');
+}
+
+alt.onServer('showChat', showChat);
+export function showChat() {
+	chatVisible = true;
+}
+
+alt.onServer('hideChat', hideChat);
+export function hideChat() {
+	chatVisible = false;
+}
+
+alt.on('keyup', (key) => {
+	if (chatVisible === false) return;
+
+    if (inputVisible === true) {
+        switch (key) {
+            case 0x1B:
+				view.emit('hideInput');
+                break;
+            case 0x26:
+                view.emit('shiftHistoryUp');
+                break;
+            case 0x28:
+                view.emit('shiftHistoryDown');
+                break;
+            case 0x0D:
+                view.emit('executeInput');
+                break;
+		}
+		
+		view.emit('countInputLength');
+    }
+
+    if (inputVisible === false) {
+        switch (key) {
+            case 0x54:
+                view.emit('showInput');
+                break;
+        }
+    }
+});
+
+view.on('onChatLoaded', () => {
+	addChatMessage('Hello there!');
+});
+
+view.on('onChatInput', (input) => {
+    alt.emitServer('onChatInput', input);
+});
+
+view.on('onChatUpdate', (chat, input) => {
+	chatVisible = chat;
+	inputVisible = input;
+
+	if (inputVisible) {
+		alt.showCursor(true);
+		alt.toggleGameControls(false);
+	} else {
+		alt.showCursor(false);
+		alt.toggleGameControls(true);
+	}
+});
---
+++ server.mjs
@@ -0,0 +1,86 @@
+import * as alt from 'alt';
+
+let registeredCommands = [];
+
+alt.onClient('onChatInput', (player, input) => {
+    let isCommand = (input[0] === '/') ? true : false;
+    
+    if (isCommand) {
+        executeCommand(player, input);
+    } else {
+        alt.emitClient(null, 'chat:Broadcast', player.name, input);
+    }
+
+    console.log(`[CHAT] ${player.name}: ${input}`);
+});
+
+function executeCommand(player, input) {
+    let commandExists = false;
+
+    registeredCommands.find((cmdArray) => {
+        let [ command, callback, requiredArgumentsCount, description, args ] = cmdArray;
+        let spaceLocation = input.search(' ');
+        let hasArguments = (spaceLocation === -1) ? false : true;
+        let userCommand = (hasArguments) ? input.substr(1, spaceLocation - 1) : input.substr(1);
+
+        if (command === userCommand) {
+            commandExists = true;
+            if (hasArguments) {
+                // Take all input and split it into array
+                let specifiedArguments = split(input, ' ', requiredArgumentsCount);
+                // Remove command text from an array
+                specifiedArguments.shift();
+                
+                // Check if has all required arguments
+                if (specifiedArguments.indexOf('') === -1) {
+                    callback(player, ...specifiedArguments);
+                } else showCommandInfoMessage(player, command, description, args);
+                // Player did specified argument(s)
+
+            } else showCommandInfoMessage(player, command, description, args);
+            return;
+        }
+    });
+
+    if(!commandExists) outputMessage(player, 'Unknown command!');
+}
+
+function showCommandInfoMessage(player, command, description, args) {
+    outputMessage(player, `> USAGE: /${command} ${args}`);
+    if (description) outputMessage(player, `> ${description}`);
+}
+
+export function onCommand(command, argumentsCount, callback, description, usage) {
+    registeredCommands.push([command, argumentsCount, callback, description, usage]);
+    alt.emitClient(null, 'chat:addCommandToChatList', command, description);
+}
+
+export function outputMessage(player, text) {
+	alt.emitClient(player, 'addChatMessage', text);
+}
+
+export function broadcast(text) {
+    alt.emitClient(null, 'addChatMessage', text);
+}
+
+export function showChat(player) {
+    alt.emitClient(player, 'showChat');
+}
+
+export function hideChat(player) {
+    alt.emitClient(player, 'hideChat');
+}
+
+/**
+ *  Utilities
+ */
+
+function split(s, separator, limit) {
+    // split the initial string using limit
+    var arr = s.split(separator, limit);
+    // get the rest of the string...
+    var left = s.substring(arr.join(separator).length + separator.length);
+    // and append it to the array
+    arr.push(left);
+    return arr;
+  }
