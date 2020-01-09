bf47eef5b0fcd140b7753c8af112eb9ee8084b10
Wanderer <vitkusdominykas@gmail.com>
2020-01-09 20:11:28 +0200

    Hotfix

--- server.mjs
+++ server.mjs
@@ -87,7 +87,7 @@ onCommand('help', (player) => {
     output(player, `{0099ff}[ List of commands ]`);
     registeredCommands.forEach((c) => {
         const [ command, _, __, description ] = c;
-        output(player, `{80ccff}${command}{b3e0ff} - ${description}`);
+        output(player, `{80ccff}/${command}{b3e0ff} - ${description}`);
     });
 }, 0, 'List of commands');
 
